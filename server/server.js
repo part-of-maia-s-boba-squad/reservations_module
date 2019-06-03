0a71e481548ebe836138a99b46b1f5851122f8dd
Roger Dunn <rrdunn@gmail.com>
2019-06-03 10:58:28 -0700

    Add Brotli compression to webpack

--- server/server.js
+++ server/server.js
@@ -3,12 +3,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const models = require('./models.js');
-
+const expressStaticGzip = require('express-static-gzip');
 const app = express();
 var port = 3010;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
+  enableBrotli: true,
+  orderPreference: ['br', 'gz'],
+  setHeaders: function (res, path) {
+    res.setHeader("Cache-Control", "public, max-age=31536000");
+   },
+   index: false
+}));
+// app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
